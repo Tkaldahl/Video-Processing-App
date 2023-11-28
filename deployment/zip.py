e99c3f5a406f1ab5f240161dd8ed0fa6896a6f7c
Tkaldahl <tkaldahl90@gmail.com>
2023-11-27 23:10:00 -0500

    add deployment script

---
+++ deployment/zip.py
@@ -0,0 +1,19 @@
+import os
+import zipfile
+
+def create_zip_with_ignore(source_folder, output_zip, ignore_files, ignore_dirs):
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(source_folder):
+            dirs[:] = [d for d in dirs if d not in ignore_dirs]
+            for file in files:
+                if file not in ignore_files:
+                    zipf.write(os.path.join(root, file), 
+                               os.path.relpath(os.path.join(root, file), 
+                               os.path.join(source_folder, '..')))
+
+# Usage
+source_folder = '.'
+output_zip = 'deployment/Video_Processing_App.zip'
+ignore_files = []
+ignore_dirs = ['.git', 'venv', 'deployment']
+create_zip_with_ignore(source_folder, output_zip, ignore_files, ignore_dirs)
