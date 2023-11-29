7b17a0b817da08b5e52acc7d1043043ea1ca399f
tkaldahl <tkaldahl90@gmail.com>
2023-11-29 18:43:24 -0500

    add boto3

--- deployment/zip.py
+++ deployment/zip.py
@@ -9,8 +9,16 @@ def create_zip_with_ignore(source_folder, output_zip, ignore_files, ignore_dirs)
                 if file not in ignore_files:
                     zipf.write(os.path.join(root, file))
 # Usage
+dependencies_source_folder = 'aws_lambda_env/lib/python3.9/site-packages'
+dependencies_output_zip = 'deployment/Dependencies.zip'
+dependencies_ignore_files = []
+dependencies_ignore_dirs = ['urllib3', 'six', 'jmespath', 'python-dateutil', 'botocore', 's3transfer', 'boto3']
+create_zip_with_ignore(dependencies_source_folder, dependencies_output_zip, dependencies_ignore_files, dependencies_ignore_dirs)
+
+
 source_folder = '.'
 output_zip = 'deployment/Video_Processing_App.zip'
 ignore_files = []
-ignore_dirs = ['.git', 'venv', 'deployment']
+ignore_dirs = ['.git', 'venv', 'deployment', 'aws_lambda_env']
+
 create_zip_with_ignore(source_folder, output_zip, ignore_files, ignore_dirs)
