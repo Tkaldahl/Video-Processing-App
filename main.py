85ba9a548ee980c36027ee2766df3a09ba39e511
tkaldahl <tkaldahl90@gmail.com>
2024-01-13 22:59:16 -0500

    update api to only accept POST

--- main.py
+++ main.py
@@ -5,36 +5,16 @@ from video_service.video_controller import VideoController  # replace with your 
 app = Flask(__name__)
 
 
-@app.route('/process_video', methods=['Get', 'POST', 'OPTIONS'])
+@app.route('/process_video', methods=['POST'])
 def process_video():
-    print("hello from process_video")
-    if (request.method == 'OPTIONS'):
-        print("hello from OPTIONS")
-        response_headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        }
-        return '', 200, response_headers
     
-    if (request.method == 'GET'):
-        print("hello from GET")
-        response_headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        }
-        return '', 200, response_headers
-    
-    if (request.method == 'POST'):
-        print("hello from POST")
+    if request.method == 'POST':
         response_headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type'
         }
 
-        # process_video_req = request.get_json()
         process_video_req = request.get_data().decode('utf-8')
         process_video_req_json = json.loads(process_video_req)
         video_urls = process_video_req_json['video_urls']
